ba8a893034c01a4535775ab33cc2b0a573277937
martingaidoo <martingaido00@gmail.com>
2025-12-12 15:35:26 -0300

    se agrega el abm de reclamos

--- src/reclamo/reclamo.controller.ts
+++ src/reclamo/reclamo.controller.ts
@@ -1,13 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile, BadRequestException } from '@nestjs/common';
 import { ReclamoService } from './reclamo.service';
 import { CreateReclamoDto } from './dto/create-reclamo.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('reclamo')
 export class ReclamoController {
     constructor(private readonly reclamoService: ReclamoService) { }
 
     @Post()
-    create(@Body() createReclamoDto: CreateReclamoDto) {
+    @UseInterceptors(FileInterceptor('file', {
+        storage: diskStorage({
+            destination: './uploads',
+            filename: (req, file, cb) => {
+                const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('');
+                return cb(null, `${randomName}${extname(file.originalname)}`);
+            }
+        })
+    }))
+    create(@Body() createReclamoDto: CreateReclamoDto, @UploadedFile() file?: any) {
+        console.log('Received Body:', createReclamoDto);
+        console.log('Received File:', file);
+        if (file) {
+            createReclamoDto.evidencia = file.path;
+        }
         return this.reclamoService.create(createReclamoDto);
     }
 
